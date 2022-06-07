4edb55793c29af4953be0b9046c91a200a2788b6
Vu Tien <tienxmin0123@gmail.com>
2022-06-07 16:46:33 +0700

    Uploads base project

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,6 @@
-import '../styles/globals.css'
+// Import css
+import '../styles/styles.scss';
 
-function MyApp({ Component, pageProps }) {
+export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
-
-export default MyApp
